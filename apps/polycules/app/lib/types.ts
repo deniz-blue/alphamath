0c0ceb8124042d2c40563a98b7311a3bcb24e054
TheAlan404 <gokdenizbektas@gmail.com>
2025-09-29 16:15:57 +0000

    [poly] update types with comments for syl

--- apps/polycules/app/lib/types.ts
+++ apps/polycules/app/lib/types.ts
@@ -14,6 +14,7 @@ export type GraphNodeRef =
     | { type: "person"; id: string }
     | { type: "system"; id: string };
 
+// A person
 export interface Person {
     id: string;
     systemId?: string;
@@ -21,26 +22,40 @@ export interface Person {
     name: string;
     avatarUrl?: string | null;
     color?: string | null;
-    website?: string | null;
+
+    // extra metadata for filtering or 3rd party tools
+    meta?: Record<string, string>;
 };
 
+// Plural system
+// each member is defined as a Person with a `systemId`
+// and is referenced back in `memberIds`
 export interface System {
     id: string;
     name: string;
     avatarUrl?: string | null;
     color?: string | null;
     memberIds: string[];
-    pluralkitId?: string | null;
+    pluralkitId?: string | null; // might remove? or stay?
 };
 
+// Two-way relationship
 export interface Relationship {
     id: string;
     from: GraphNodeRef;
     to: GraphNodeRef;
+
     label?: string;
+
+    // might expand!
 };
 
+// A relationship that is a group
+// Not exactly implemented anywhere *yet*
+// i.e friend groups, polycules where everyone is with each other etc.
 export interface GroupRelationship {
     id: string;
     memberIds: string[];
+
+    // might expand!
 };
