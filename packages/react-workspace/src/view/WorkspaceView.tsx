5553bdcc7b1c8a3f61f9d4e31a2854c840020875
TheAlan404 <gokdenizbektas@gmail.com>
2025-10-01 14:04:13 +0000

    [react-workspace] use viewBox instead of g:transform

--- packages/react-workspace/src/view/WorkspaceView.tsx
+++ packages/react-workspace/src/view/WorkspaceView.tsx
@@ -1,5 +1,6 @@
-import React, { forwardRef, PropsWithChildren, useContext } from "react";
+import React, { PropsWithChildren, useContext } from "react";
 import { GlobalTransform } from "../core/GlobalTransformContext.js";
+import { mergeRefs, useElementSize } from "@mantine/hooks";
 
 export type WorkspaceViewProps = PropsWithChildren<React.JSX.IntrinsicElements["svg"]>;
 
@@ -8,14 +9,21 @@ export const WorkspaceView = ({
     ref,
     ...props
 }: WorkspaceViewProps) => {
+    const { ref: sizeRef, height, width } = useElementSize();
     const { position, scale } = useContext(GlobalTransform);
 
     return (
         <svg
             {...props}
-            ref={ref}
+            ref={mergeRefs(ref, sizeRef)}
             width="100%"
             height="100%"
+            viewBox={[
+                (-position.x) / scale,
+                (-position.y) / scale,
+                width / scale,
+                height / scale,
+            ].join(" ")}
             style={{
                 overflow: "hidden",
                 position: "fixed",
@@ -26,9 +34,7 @@ export const WorkspaceView = ({
                 ...(props.style || {}),
             }}
         >
-            <g
-                transform={`translate(${position.x}, ${position.y}) scale(${scale})`}
-            >
+            <g>
                 {children}
             </g>
         </svg>
